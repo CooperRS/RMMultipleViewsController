ad2ea3c0c35c58028f9232041f00f3ec06e54af2
CooperRS <cooperrs@gmail.com>
2017-09-14 23:38:48 +0200

    Add table view to demo project

---
+++ RMMultipleViewsController-Demo/RMDemoThirdViewController.m
@@ -0,0 +1,17 @@
+//
+//  RMDemoThirdViewController.m
+//  RMMultipleViewsController-Demo
+//
+//  Created by Roland Moers on 14.09.17.
+//  Copyright © 2017 Roland Moers. All rights reserved.
+//
+
+#import "RMDemoThirdViewController.h"
+
+@interface RMDemoThirdViewController ()
+
+@end
+
+@implementation RMDemoThirdViewController
+
+@end
